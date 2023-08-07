282a88ef51aa1a166fb7e4e6b77a7c0f11f3ffcd
2udy <22udy629@gmail.com>
2023-08-07 22:39:13 +0900

    section02 2번 수정, 4번 풀이

--- section02/01_sol02.py
+++ section02/01_sol02.py
@@ -1,8 +1,6 @@
 '''
 K 번째 약수
-80/100
-15번째 라인 K번째 약수가 N인 경우오류 발생
-K번째 약수가 N보다 큰지 확인한 후에 i+1 해야한다.
+100/100
 '''
 
 N, K = map(int, input().split())
@@ -13,10 +11,10 @@ while K != 0:
     if N % i == 0:
         K -= 1
         ans = i
-    i += 1
 
     if N % i == N:
         ans = -1
         break
+    i += 1
 
 print(ans)
